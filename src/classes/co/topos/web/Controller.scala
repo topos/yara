e9011f72827a2c5a1d019b0b25b7642e69d85fb4
ml <slacket@gmail.com>
2012-02-25 03:28:07 -0500

    check the type of request

--- src/classes/co/topos/web/Controller.scala
+++ src/classes/co/topos/web/Controller.scala
@@ -4,5 +4,9 @@ abstract class Controller(val context: WebContext) {
   val request = context.request
   val response = context.response
 
+  def isPost = request.getMethod == "POST"
+  def isGet = request.getMethod == "GET"
+  def isPut = request.getMethod == "PUT"
+
   def service(): Unit
 }
